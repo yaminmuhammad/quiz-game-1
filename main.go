12be8934b3a298c8ff64e6efb9e418270cb18237
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-12-02 12:45:24 +0700

    fix function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -40,7 +41,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
